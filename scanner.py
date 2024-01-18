66dddb78e0efd0bf620fd9f6402d139253e53c1d
latews <latews77@gmail.com>
2024-01-18 13:33:43 +0300

    added banner

--- scanner.py
+++ scanner.py
@@ -5,12 +5,27 @@ import ipaddress
 import argparse
 from concurrent.futures import ThreadPoolExecutor
 
+print("""
+                          888                                                                       
+                          888                                                                       
+                          888                                                                       
+88888b.   .d88b.  888d888 888888      .d8888b   .d8888b  8888b.  88888b.  88888b.   .d88b.  888d888 
+888 "88b d88""88b 888P"   888         88K      d88P"        "88b 888 "88b 888 "88b d8P  Y8b 888P"   
+888  888 888  888 888     888         "Y8888b. 888      .d888888 888  888 888  888 88888888 888     
+888 d88P Y88..88P 888     Y88b.            X88 Y88b.    888  888 888  888 888  888 Y8b.     888     
+88888P"   "Y88P"  888      "Y888       88888P'  "Y8888P "Y888888 888  888 888  888  "Y8888  888     
+888                                                                                                 
+888                                                                                                
+888
+                                          latews                 
+      """)
+
 def scan_target(ip, port):
     try:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.settimeout(1)
             s.connect((ip, port))
-            print(f"[+] {ip}:{port} - Open")
+            print(f"[+] {ip} -> {port} - Open")
     except (socket.timeout, socket.error):
         pass
 
@@ -37,4 +52,5 @@ def args():
     scan(args.ip, args.port)
 if __name__ == "__main__":
     args()
+
 
